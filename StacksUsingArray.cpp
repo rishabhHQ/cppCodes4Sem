e3c713a5d6031a3ca472d78baa14c00405628d63
rishabh-24bcs10110 <24bcs10110@cuchd.in>
2026-01-18 21:14:21 +0530

    created a new repo for 4th sem DAA cpp codes

---
+++ StacksUsingArray.cpp
@@ -0,0 +1,15 @@
+/******************************************************************************
+Implement a stack using an array. Perform the following operations: push(x), pop(), top()
+(or peek()), isEmpty(), and isFull() operations, and prints output based on
+query inputs.
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World";
+
+    return 0;
+}
