f16c7897802a38ddd3ee5e8307337c0703eaabb5
rishabh-24bcs10110 <24bcs10110@cuchd.in>
2026-01-19 01:23:42 +0530

    edited cpp stacks file

--- StacksUsingArray.cpp
+++ StacksUsingArray.cpp
@@ -5,11 +5,42 @@ query inputs.
 
 *******************************************************************************/
 #include <iostream>
+#include <vector>
 using namespace std;
+
+
+vector<int> pushInArrey(vector <int> arrey, int size) {
+
+    for (int i = 0; i<size; i++) {
+        arrey.push_back(i);
+    }
+    return arrey;
+}
+
+// vector<int> popOutArrey(vector <int> arrey, int size) {
+    
+//     if (!isEmpty) {
+//         arrey.pop_back();
+//     }
+// }
+
+// bool isEmpty(vector <int> arrey) {
+    
+//     if (arrey.top == 0) {
+//         return true;
+//     }
+// }
 
 int main()
 {
-    cout<<"Hello World";
+    int i,size;
+
+    cout << "Enter size of array: ";
+    cin >> size;
+
+    // vector<int> arrey(size);
+    
+       
 
     return 0;
 }
